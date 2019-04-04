3adda8936c42c9bf388869d3d3b6e4223df91cab
Jeff Langston <info@jefflangston.ca>
2019-04-04 15:06:02 -0700

    added logo to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import logo from "../images/Transparent.svg"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div style={{ marginBottom: `1.45rem` }}>
+      <img src={logo} alt="Logo" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const fluidImage = graphql`
+  fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "Transparent.svg" }) {
+      ...fluidImage
+    }
+  }
+`
 
 export default IndexPage
