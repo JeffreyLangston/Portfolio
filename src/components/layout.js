4383921df9010309280a7614bc801400284fb547
Jeff Langston <jeffl@kimon.io>
2019-08-07 22:53:12 -0600

    changed to mate gatsby template

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,45 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React, { Fragment } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+import { ScrollingProvider } from 'react-scroll-section';
+import 'react-tippy/dist/tippy.css';
+import config from 'react-reveal/globals';
+import colors from '../../colors';
+import Helmet from './Helmet';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+const GlobalStyle = createGlobalStyle`
+*,
+*::after,
+*::before { 
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+  }
 
-import Header from "./header"
-import "./layout.css"
+body {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box; 
+  margin: 0;
+  font-family: Cabin;
+  overflow-x: hidden;
+}
+`;
+
+config({ ssrFadeout: true });
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-        </div>
-      </>
-    )}
-  />
-)
+  <Fragment>
+    <GlobalStyle />
+    <ThemeProvider theme={{ colors }}>
+      <ScrollingProvider>
+        <Helmet />
+        {children}
+      </ScrollingProvider>
+    </ThemeProvider>
+  </Fragment>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,21 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-
-import Layout from "../components/layout"
-import logo from "../images/Transparent.svg"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../components/Layout';
+import Landing from '../sections/Landing';
+import About from '../sections/About';
+import Projects from '../sections/Projects';
+import Writing from '../sections/Writing';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-
-    <div style={{ marginBottom: `1.45rem` }}>
-      <img src={logo} alt="Logo" />
-    </div>
+    <Header />
+    <Landing />
+    <About />
+    <Projects />
+    <Writing />
+    <Footer />
   </Layout>
-)
+);
 
-export const fluidImage = graphql`
-  fragment fluidImage on File {
-    childImageSharp {
-      fluid(maxWidth: 1000) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-`
-
-export const pageQuery = graphql`
-  query {
-    imageOne: file(relativePath: { eq: "Transparent.svg" }) {
-      ...fluidImage
-    }
-  }
-`
-
-export default IndexPage
+export default IndexPage;
