396960b3d1c19be25987581692568aa645b2f4e6
Jeff Langston <jeffl@kimon.io>
2019-08-08 01:48:47 -0600

    Test for casing fix.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Landing from '../sections/Landing';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
 import Writing from '../sections/Writing';
-import Header from '../components/Header';
+import Header from '../components/header';
 import Footer from '../components/Footer';
 
 const IndexPage = () => (
